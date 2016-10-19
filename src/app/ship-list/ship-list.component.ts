936e74a8f4b9b0a52ee7d5df7a0c7795ac8b01c7
Rashmi Dey <rashmidey@NLIFSMPMOU7G3QN.local>
2016-10-19 09:11:56 -0500

    tryinh async pipes

--- src/app/ship-list/ship-list.component.ts
+++ src/app/ship-list/ship-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {StarshipService, IShip} from '../starship.service'; // import here to use the class name in constructor
 import { Observable } from 'rxjs/Observable'; // importing the Observable type only. We do the other import syntax to import the sideeffects of Observables
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'ship-list',
@@ -10,12 +11,16 @@ import { Observable } from 'rxjs/Observable'; // importing the Observable type o
 export class ShipListComponent implements OnInit {
   //ships: IShip[];
   ships: Observable<IShip[]>;
+  length: number;
   constructor(ss: StarshipService) {
     //this.ships = ss.getStarships();
     // ss.getStarships().subscribe((s: IShip[]) => {
     //   this.ships = s;
     // })
-    this.ships = ss.getStarships();
+    this.ships = ss.getStarships().do(x => this.length = x.length);
+    //this.ships.map(x => this.length = x.length);
+    console.log("-----------------");
+    console.log(this.length);
    }
 
   ngOnInit() {

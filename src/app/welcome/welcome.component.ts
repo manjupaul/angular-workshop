8a821a26c1d244a1e2a94178057965f58e395b2c
Rashmi Dey <rashmidey@NLIFSMPMOU7G3QN.local>
2016-10-19 09:45:03 -0500

    routeDriveDataLoading

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/switchMap';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-welcome',
@@ -8,16 +11,22 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-
-  constructor(title: Title, private route: ActivatedRoute) {
-    console.log("setting title");
-    title.setTitle("Welcome");
-
+  pageTitle: string;
+  message: string;
+  personInfo: Observable<{}>;
+  constructor(title: Title, private route: ActivatedRoute, http: Http) {
+    console.log(title.getTitle());
+    title.setTitle('Welcome');
+    this.pageTitle = title.getTitle();
+    console.log(document.title);
     this.route.params.subscribe(params => {
-      console.log("Params: ", params);
-      console.log("Welcoming " + params['who']);
+      console.log('Got new value for params', params);
+      this.message = params['who'];
     });
-   }
+    this.personInfo = this.route.params.map(p => p['who']).switchMap(n => {
+      return http.get('/assets/' + n + '.json').map(r => r.json());
+    });
+  }
 
   ngOnInit() {
   }
